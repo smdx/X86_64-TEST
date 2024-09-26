ed6b99af2a1e3fe1a68aea3225a9c8c395457e91
MenGQiu <smdx02@gmail.com>
2024-09-26 14:11:42 +0800

    Add files via upload

---
+++ scripts/PKG_Version_Get.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Get Package Versions
+adguardhome_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep adguardhome | head -n 1))
+mosdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep mosdns | head -n 1))
+smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-smartdns/Makefile)
+passwall_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
+passwall2_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall2/Makefile)
+store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep store | head -n 1))
+echo "adguardhome=$adguardhome_version" >> $GITHUB_ENV
+echo "mosdns=$mosdns_version" >> $GITHUB_ENV
+echo "smartdns=$smartdns_version" >> $GITHUB_ENV
+echo "passwall_version=$passwall_version" >> $GITHUB_ENV
+echo "passwall2_version=$passwall2_version" >> $GITHUB_ENV
+echo "store=$store_version" >> $GITHUB_ENV
---
+++ scripts/PKG_Version_Output.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Package Versions Output
+echo "管理地址：10.0.0.1" >> release.txt
+echo "默认密码：空" >> release.txt
+echo "插件清单：AdGuard Home、MosDNS、SmartDNS、Passwall、Passwall2、Store、Wol、MultiSD_Lite、udpxy、UPnP\n" >> release.txt
+echo "Adguardhome Version: $adguardhome" >> release.txt
+echo "MosDNS Version: $mosdns" >> release.txt
+echo "SmartDNS Version: $smartdns" >> release.txt
+echo "Passwall Version: $passwall_version" >> release.txt
+echo "Passwall2 Version: $passwall2_version" >> release.txt
+echo "Store Version: $store" >> release.txt
