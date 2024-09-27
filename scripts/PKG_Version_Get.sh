2c17583285f34beae00e00dbcf18a3bf34bd33c5
MenGQiugit config --global user.namegit config --global user.email smdx02@gmail.comgit config --global user.email <smdx02@gmail.com>
2024-09-28 02:57:35 +0800

    test PKG_Version

--- scripts/PKG_Version_Get.sh
+++ scripts/PKG_Version_Get.sh
@@ -5,10 +5,12 @@ mosdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | 
 smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-smartdns/Makefile)
 passwall_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
 passwall2_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall2/Makefile)
-store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-store/Makefile)
+openclash_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-openclash/Makefile)
+store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep store | head -n 1))
 echo "adguardhome=$adguardhome_version" >> $GITHUB_ENV
 echo "mosdns=$mosdns_version" >> $GITHUB_ENV
 echo "smartdns=$smartdns_version" >> $GITHUB_ENV
 echo "passwall_version=$passwall_version" >> $GITHUB_ENV
 echo "passwall2_version=$passwall2_version" >> $GITHUB_ENV
+echo "openclash=$openclash_version" >> $GITHUB_ENV
 echo "store=$store_version" >> $GITHUB_ENV
--- scripts/PKG_Version_Output.sh
+++ scripts/PKG_Version_Output.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 # Package Versions Output
-echo "管理地址：10.0.0.1" >> release.txt
-echo "默认密码：空" >> release.txt
-echo "插件清单：AdGuard Home、MosDNS、SmartDNS、Passwall、Passwall2、Store、Wol、MultiSD_Lite、udpxy、UPnP" >> release.txt
-echo "---------------------------------------------" >> release.txt
-echo "Adguardhome Version: $adguardhome" >> release.txt
-echo "MosDNS Version: $mosdns" >> release.txt
-echo "SmartDNS Version: $smartdns" >> release.txt
-echo "Passwall Version: $passwall_version" >> release.txt
-echo "Passwall2 Version: $passwall2_version" >> release.txt
-echo "Store Version: $store" >> release.txt
+
+if [[ $REPO_URL == *"lede"* ]]; then
+    # 如果地址包含"lede"，执行以下操作
+    echo "管理地址：10.0.0.1" >> release.txt
+    echo "默认密码：空" >> release.txt
+    echo "插件清单：AdGuard Home、MosDNS、SmartDNS、Passwall、Passwall2、OpenClash、Store、Wol、udpxy、UPnP" >> release.txt
+    echo "---------------------------------------------" >> release.txt
+    echo "Adguardhome Version: $adguardhome" >> release.txt
+    echo "MosDNS Version: $mosdns" >> release.txt
+    echo "SmartDNS Version: $smartdns" >> release.txt
+    echo "Passwall Version: $passwall_version" >> release.txt
+    echo "Passwall2 Version: $passwall2_version" >> release.txt
+    echo "OpenClash Version: $openclash" >> release.txt
+    echo "Store Version: $store" >> release.txt
+elif [[ $REPO_URL == *"immortalwrt"* ]]; then
+    # 如果地址包含"immortalwrt"，执行以下操作
+    echo "管理地址：10.0.0.1" >> release.txt
+    echo "默认密码：空" >> release.txt
+    echo "插件清单：AdGuard Home、MosDNS、SmartDNS、Passwall、Passwall2、Store、Wol、MultiSD_Lite、udpxy、UPnP" >> release.txt
+    echo "---------------------------------------------" >> release.txt
+    echo "Adguardhome Version: $adguardhome" >> release.txt
+    echo "MosDNS Version: $mosdns" >> release.txt
+    echo "SmartDNS Version: $smartdns" >> release.txt
+    echo "Passwall Version: $passwall_version" >> release.txt
+    echo "Passwall2 Version: $passwall2_version" >> release.txt
+fi
