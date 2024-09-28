4dcfe0cfcb3ecbfbc0f8097a5af67c8124d7b0b6
MenGQiugit config --global user.namegit config --global user.email smdx02@gmail.comgit config --global user.email <smdx02@gmail.com>
2024-09-28 10:01:45 +0800

    更新 PKG_Version_Get.sh

--- scripts/PKG_Version_Get.sh
+++ scripts/PKG_Version_Get.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Get Package Versions
 adguardhome_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep adguardhome | head -n 1))
-mosdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep mosdns | head -n 1))
+mosdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/mosdns/Makefile)
 smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-smartdns/Makefile)
 passwall_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
 passwall2_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall2/Makefile)
 openclash_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-openclash/Makefile)
-store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' $(find feeds/ -name Makefile | grep store | head -n 1))
+store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-store/Makefile)
 echo "adguardhome=$adguardhome_version" >> $GITHUB_ENV
 echo "mosdns=$mosdns_version" >> $GITHUB_ENV
 echo "smartdns=$smartdns_version" >> $GITHUB_ENV
