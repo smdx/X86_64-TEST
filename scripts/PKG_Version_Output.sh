22b2633d29b717192c0a5424b88beb1ffb98215d
MenGQiu <smdx02@gmail.com>
2024-09-28 11:54:58 +0800

    Update PKG_Version_Output.sh

--- scripts/PKG_Version_Output.sh
+++ scripts/PKG_Version_Output.sh
@@ -6,7 +6,7 @@ if [[ $REPO_URL == *"lede"* ]]; then
     echo "Openwrt Lean-x86" >> release.txt
     echo "管理地址：10.0.0.1" >> release.txt
     echo "默认密码：空" >> release.txt
-    #echo "插件清单：AdGuard Home、MosDNS、SmartDNS、Passwall、Passwall2、OpenClash、Store、Vlmcsd、Qos-Sqm、Wol、natmap、udpxy、UPnP" >> release.txt
+    echo "插件清单：AdGuard Home、MosDNS、SmartDNS、Passwall、Passwall2、OpenClash、Store、Vlmcsd、Qos-Sqm、Wol、natmap、udpxy、UPnP" >> release.txt
     echo "---------------------------------------------" >> release.txt
     echo "Adguardhome Version: $adguardhome" >> release.txt
     echo "MosDNS Version: $mosdns" >> release.txt
