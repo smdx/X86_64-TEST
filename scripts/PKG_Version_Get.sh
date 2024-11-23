d2ff06cb6fe55b442edb64f7aea86c8ab153e9ba
MenGQiugit config --global user.namegit config --global user.email smdx02@gmail.comgit config --global user.email <smdx02@gmail.com>
2024-11-23 21:28:48 +0800

    Update PKG_Version_Get.sh

--- scripts/PKG_Version_Get.sh
+++ scripts/PKG_Version_Get.sh
@@ -2,15 +2,20 @@
 # Package  Version Output to Env
 adguardhome_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/adguardhome/Makefile)
 mosdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' package/mosdns/mosdns/Makefile)
-smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-smartdns/Makefile)
+mosdns_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' package/mosdns/mosdns/Makefile)
+smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/smartdns/Makefile)
+smartdns_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/kenzo/smartdns/Makefile)
 passwall_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
+passwall_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
 passwall2_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall2/Makefile)
+passwall2_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/small/luci-app-passwall2/Makefile)
 openclash_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-openclash/Makefile)
+openclash_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/small/luci-app-openclash/Makefile)
 store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-store/Makefile)
 echo "adguardhome=$adguardhome_version" >> $GITHUB_ENV
-echo "mosdns=$mosdns_version" >> $GITHUB_ENV
-echo "smartdns=$smartdns_version" >> $GITHUB_ENV
-echo "passwall_version=$passwall_version" >> $GITHUB_ENV
-echo "passwall2_version=$passwall2_version" >> $GITHUB_ENV
-echo "openclash=$openclash_version" >> $GITHUB_ENV
+echo "mosdns=${mosdns_version}-${mosdns_release}" >> $GITHUB_ENV
+echo "smartdns=Release ${smartdns_version}-${smartdns_release}" >> $GITHUB_ENV
+echo "passwall_version=${passwall_version}-${passwall_release}" >> $GITHUB_ENV
+echo "passwall2_version=${passwall2_version}-${passwall2_release}" >> $GITHUB_ENV
+echo "openclash=${openclash_version}-${openclash_release}" >> $GITHUB_ENV
 echo "store=$store_version" >> $GITHUB_ENV
